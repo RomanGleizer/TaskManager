1c7e957ad8142fe9d29e4de37dc714d8a6f64754
Roman <125725530+RomanGleizer@users.noreply.github.com>
2024-03-09 15:31:33 +0500

    DTOs

---
+++ Logic/Dto/RoleDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace Logic.Dto;
+
+/// <summary>
+/// Представляет объект передачи данных (DTO) роли
+/// </summary>
+public record RoleDTO
+{
+    /// <summary>
+    /// Получает или устанавливает идентификатор роли
+    /// </summary>
+    public required int Id { get; init; }
+
+    /// <summary>
+    /// Получает или устанавливает название роли
+    /// </summary>
+    public required string Name { get; init; }
+
+    /// <summary>
+    /// Получает или устанавливает список идентификаторов пользователей, связанных с данной ролью
+    /// </summary>
+    public required IList<string> UserIds { get; init; }
+}

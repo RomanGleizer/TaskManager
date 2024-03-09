651343e31d4d79d9dbe842fe3b9771c0be23a5e1
Roman <125725530+RomanGleizer@users.noreply.github.com>
2024-03-09 13:10:39 +0500

    User/Role dal models

---
+++ IdentityService/Dal/Entities/RoleDal.cs
@@ -0,0 +1,30 @@
+﻿using Core.Dal.Base;
+using Microsoft.AspNetCore.Identity;
+
+namespace Dal.Entities;
+
+/// <summary>
+/// Представляет сущность роли в базе данных
+/// </summary>
+public class RoleDal : IdentityRole, IBaseEntity<int>
+{
+    /// <summary>
+    /// Получает или устанавливает идентификатор роли
+    /// </summary>
+    public required new int Id { get; init; }
+
+    /// <summary>
+    /// Получает или устанавливает название роли.
+    /// </summary>
+    public required new string Name { get; init; }
+
+    /// <summary>
+    /// Получает или устанавливает список идентификаторов пользователей, связанных с данной ролью
+    /// </summary>
+    public required IList<string> UserIds { get; init; }
+
+    /// <summary>
+    /// Получает или устанавливает список пользователей, связанных с данной ролью
+    /// </summary>
+    public required IList<UserDal> Users { get; init; }
+}
---
+++ IdentityService/Dal/Entities/UserDal.cs
@@ -0,0 +1,55 @@
+﻿using Core.Dal.Base;
+using Microsoft.AspNetCore.Identity;
+
+namespace Dal.Entities;
+
+/// <summary>
+/// Представляет сущность пользователя в базе данных
+/// </summary>
+public class UserDal : IdentityUser, IBaseEntity<Guid>
+{
+    /// <summary>
+    /// Получает или устанавливает идентификатор пользователя
+    /// </summary>
+    public required new Guid Id { get; init; }
+
+    /// <summary>
+    /// Получает или устанавливает имя пользователя
+    /// </summary>
+    public required string FirstName { get; init; }
+
+    /// <summary>
+    /// Получает или устанавливает фамилию пользователя
+    /// </summary>
+    public required string LastName { get; init; }
+
+    /// <summary>
+    /// Получает или устанавливает электронную почту пользователя
+    /// </summary>
+    public required new string Email { get; init; }
+
+    /// <summary>
+    /// Получает или устанавливает номер телефона пользователя
+    /// </summary>
+    public required new string PhoneNumber { get; init; }
+
+    /// <summary>
+    /// Получает или устанавливает дату рождения пользователя
+    /// </summary>
+    public required DateTime BirthDay { get; init; }
+
+    /// <summary>
+    /// Получает или устанавливает идентификатор роли пользователя
+    /// </summary>
+    public required int RoleId { get; init; }
+
+    /// <summary>
+    /// Получает или устанавливает роль пользователя
+    /// </summary>
+    public required RoleDal Role { get; set; }
+
+    /// <summary>
+    /// Получает или устанавливает список идентификаторов проектов, в которых состоит пользователь
+    /// </summary>
+    public required IList<int> ProjectIds { get; init; }
+}
